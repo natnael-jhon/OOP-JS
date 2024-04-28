42f9de7b8361302ea46d515f0b0f579e0d602cd9
NaTi Jhon <yohannesnatanel00@gamil.com>
2024-04-28 19:43:13 +0300

    Built-in constructor Done

---
+++ 04. Built-in Constructors/app.js
@@ -0,0 +1,11 @@
+// Built-in constructors, also known as native constructors, are standard constructors provided by JavaScript that allow you to create objects of various data types. These constructors are available globally and do not need to be explicitly defined. They provide a convenient way to create instances of primitive data types and built-in objects.
+
+// --> Num Constructor
+let num1 = new Number(1)
+// --> Str constructor
+let str1 = new String("Nati")
+// --> Boolean Constructor
+let bool1 = new Boolean(true)
+// --> arry Constructor
+let ary1 = new Array(1, 2, 3)
+console.log(ary1)
