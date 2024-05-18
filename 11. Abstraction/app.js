d3903c0fb31faf3e470935191c36cba9569c3ee6
NaTi Jhon <yohannesnatanel00@gamil.com>
2024-05-18 22:41:08 +0300

    Abstraction Done

--- 11. Abstraction/app.js
+++ 11. Abstraction/app.js
@@ -32,3 +32,10 @@ class Cat extends Animal{
 
 let bingo = new Dog("bingo")
 console.log(`this is my dog ${bingo.name} he syas ${bingo.makeSound()}`)
+
+// ---------- Define
+// In this example, we have an abstract class Animal that contains an abstract method makeSound(). The makeSound() method is declared in the Animal class but doesn't provide an implementation. It's meant to be implemented by subclasses like Dog and Cat.
+
+// The Dog and Cat classes are concrete subclasses that inherit from the Animal class. They implement the abstract method makeSound() with their own specific implementations for making sounds.
+
+// By using abstraction, we define a common interface (makeSound()) that each subclass must implement, while hiding the internal details of how each animal makes its sound. This simplifies the code and allows us to work with animals at a higher level of abstraction, without worrying about the specifics of each animal's sound implementation.
