0f41c19419ec6c399bbb61b409db4c74a30fe880
NaTi Jhon <yohannesnatanel00@gamil.com>
2024-05-18 22:40:04 +0300

    Inheritance Done

--- 12. Inheritance/app.js
+++ 12. Inheritance/app.js
@@ -1,75 +1,58 @@
-// -------------- ES5 Code
-// // Superclass constructor
-// function Animal(name) {
+// // -------------- ES5 Code
+// // // Superclass constructor
+// function Animal(name){
 //   this.name = name;
 // }
 
-// // Method shared among all Animal instances
-// Animal.prototype.makeSound = function () {
-//   return "Unknown sound";
-// };
-
-// // Subclass constructor inheriting from Animal
-// function Dog(name) {
-//   Animal.call(this, name); // Call the superclass constructor
+// // method shared among all animal instances 
+// Animal.prototype.makeSound = function(){
+//   return `Unknow sound`
 // }
 
-// // Set up prototype chain for Dog to inherit from Animal
-// Dog.prototype = Object.create(Animal.prototype);
+// // Subclass construcotr inheriting from Animal
+// function Dog(name){
+//   Animal.call(this, name); // call the superclass construcor 
 
-// // Method specific to Dog
+// }
+// // Setup prototype chain for dog to inherit from animal 
+// Dog.prototype = Object.create(Animal.prototype)
+
+// // Method spercific to Dog
 // Dog.prototype.makeSound = function () {
-//   return "Woof!";
-// };
+//   return 'woof!'
+// }
 
-// // Creating instances of the classes
-// const genericAnimal = new Animal("Generic Animal");
-// console.log(genericAnimal.name); // Output: Generic Animal
-// console.log(genericAnimal.makeSound()); // Output: Unknown sound
+// // Creating Instances of the classes
+// const bingo = new Dog("Bingo");
 
-// const dog = new Dog("Buddy");
-// console.log(dog.name); // Output: Buddy
-// console.log(dog.makeSound()); // Output: Woof!
+// console.log(bingo.makeSound())
 
-// ------------- Refactoring to Classes
+// ES6
 // Superclass
-class Animal {
-  constructor(name) {
+class Animal{
+  constructor(name){
     this.name = name;
+
   }
 
-  // Method shared among all Animal instances
-  makeSound() {
-    return "Unknown sound";
-  }
-}
 
-// Subclass inheriting from Animal
-class Dog extends Animal {
-  constructor(name) {
-    super(name); // Call the superclass constructor
-  }
-
-  // Method specific to Dog
-  makeSound() {
-    return "Woof!";
+  makeSound(){
+    return "unknown sound"
   }
 }
 
-// Creating instances of the classes
-const genericAnimal = new Animal("Generic Animal");
-console.log(genericAnimal.name); // Output: Generic Animal
-console.log(genericAnimal.makeSound()); // Output: Unknown sound
 
-const dog = new Dog("Buddy");
-console.log(dog.name); // Output: Buddy
-console.log(dog.makeSound()); // Output: Woof!
+// subclass inheriting from Animal superclass
 
-// ---------------- Define
-// Animal defined using a constructor function. The Animal prototype has a makeSound() method that is shared among all instances of Animal.
+class Dog extends Animal{
+  constructor(name){
+    super(name)
+  }
 
-// The subclass Dog is created by defining another constructor function that inherits from Animal. We use Object.create() to set up the prototype chain so that Dog.prototype inherits from Animal.prototype. This establishes the inheritance relationship.
+  makeSound() {
+    return  `Woff!`
+  }
+}
 
-// The Dog prototype also has its own makeSound() method, which overrides the makeSound() method from the Animal prototype. This demonstrates how the Dog subclass can provide its specific implementation for the makeSound() method while reusing other functionalities from the Animal superclass.
-
-// With this setup, instances of Dog can access both the properties and methods of the Animal superclass, as well as any methods that have been specifically defined for the Dog subclass.
+const bigro = new Dog("bigro")
+console.log(bigro.makeSound())
